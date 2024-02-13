57806ca902593481869134e348bf78c3bc5fb7a8
Akhmad Khanif Zyen <khanif.zyen@gmail.com>
2024-02-13 10:19:17 +0700

    add riverpod

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:flutter/material.dart';
+
+part 'main.g.dart';
+
+@riverpod
+String helloWorld(HelloWorldRef ref) {
+  return 'Hello World';
+}
+
+void main() {
+  runApp(const ProviderScope(
+    child: MyApp(),
+  ));
+}
+
+class MyApp extends ConsumerWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final String value = ref.watch(helloWorldProvider);
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        body: Center(
+          child: Text(value),
+        ),
+      ),
+    );
+  }
+}
